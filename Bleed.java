e5f9f92df304865e2ce2051f48ab409bb58736b5
daydreamsamurai <daydreamsamurai@gmail.com>
2020-05-28 09:14:10 -0700

    Add files via upload

--- Bleed.java
+++ Bleed.java
@@ -2,35 +2,37 @@ import java.util.*;
 public class Bleed {
 	
 	private String list; //contains current list of targets with bleed and duration of buff.
+	private String nList; //updating new list values
 	private Scanner scan = new Scanner(list);
+	
 			
 	//takes in a string with the targets identification and duration 
 	public void addTarget(String tar, int dur) {
 		list += tar + ":" + Integer.toString(dur) + "\n"; // adds new target to list
-		
+
 	}
 	
 	//runs through list of targets with debuffs
 	public void action( ) {
-		
 		while(scan.hasNext()) {
-			// split method
+			String temp = scan.next();
+			String[] aStr = temp.split(":", 2);
+			
+			//target.dmg.(1); need some way to dmg other objecs here here.
+		
+		this.update(aStr[0], Integer.parseInt(aStr[1])-1); // adds the target and decrements debuff dur to nlist
 		}
-		update();
+		list = nList; // once list has been parsed updates it with nlist taking care of anyremovals and updates.
 		scan = new Scanner(list);
 		}
 	
-	// decrements the duration of the debuff and removes once it reaches 0
-	private void update() {
-		String nList; //updating new list values
-		while(scan.hasNext()) {
-			// and split here Integer.parseInt();
+	// adds to nlist or removes tar once it reaches dur 0 
+	private void update(String tar, int dur) {
 			if(dur == 0) {
 				
 			} else {
-				nlist += target + ":" + Integer.toString(dur) + "\n";
+				nList += tar + ":" + Integer.toString(dur) + "\n";
 			}
 			
-	}
 	}
 }
