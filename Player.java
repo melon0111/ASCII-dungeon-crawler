fe61160247d5bfaebbd9aac88307e0845210bd69
daydreamsamurai <daydreamsamurai@gmail.com>
2020-05-29 10:50:37 -0700

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,10 +1,12 @@
+import java.util.Random;
 
 public class Player {
-	private int x;
-	private int y;
+	private String cord;
 	private int hp = 100;
 	private int def;
 	private int attack;
+	private boolean bleed;
+	private boolean confusion;
 	
 	//method to damage hp of the character
 	public void dmg (int x) {
@@ -18,19 +20,60 @@ public class Player {
 		}
 	}
 	
-	public void move(char symbol) {
-		if(!Dungeon.checkNewPos((x + "-" + y), symbol)) {
-			System.out.println("You hug the wall, it feels appreciated");
-		}else {
-			Dungeon.changePos((x + "-" + y), symbol);
+	//beginning of player action
+	//takes in a char representing movement, handles any special case debuffs
+	// then calls move with any changes.
+	public void action(char symbol) {
+		if(symbol >= 65 && symbol <= 90) {
+			symbol += 22; //removes capitalization from equation
+		}
+		symbol = status(symbol);
+		if(symbol == 'n' || symbol == 's' || symbol || 'w' || symbol == 'e') {
+			move(symbol);
 		}
 	}
 	
+	
+	//changes value based on value of weapon
 	public void equip(char type, int x) {
 		if(type == 'w') {
 			attack = x;
 		} else {
 			def = x;
 		}
+	}
+	
+	//takes in a symbol from action and checks if the movement is possible
+	// before updating map/cord
+	private void move(char symbol) {
+		if(!Dungeon.checkNewPos((cord), symbol)) {
+			System.out.println("You hug the wall, it feels appreciated");
+		}else {
+			cord = Dungeon.changePos((cord), symbol);
+		}
+	}
+	
+	private char status(char symbol) {
+		//checks if player has bleed debuff and damages their hp
+		if(bleed) {
+			hp -= debuff.bleed;
+			System.out.println("You take " + debuff.bleed + " in bleed damage.");
+		}
+		
+		//if player has confusion randomizes direction 
+		if(confusion) {
+			Random ran = new Random();
+			int temp = ran.nextInt(3);
+			if(temp == 0) {
+				symbol = 'n';
+			}else if(temp == 1) {
+				symbol = 's';
+			} else if(temp == 2) {
+				symbol = 'w';
+			} else {
+				symbol = 'e';
+			}
+		}
+		return symbol;
 	}
 }
